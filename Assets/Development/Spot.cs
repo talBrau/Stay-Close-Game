30768983e3e34ce9bd47075f4d3a7d5664952411
Arnon Guttel <93203059+ArnonGuttel@users.noreply.github.com>
2022-05-08 20:16:58 +0300

    Changed logic

--- Assets/Development/Spot.cs
+++ Assets/Development/Spot.cs
@@ -1,23 +1,42 @@
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
 
 public class Spot : MonoBehaviour
 {
+    #region Inspector
+    
+    [SerializeField] private UnityEvent spotEvent;
+
+    #endregion
+
+    #region Fields
+
+    #endregion
+    
+    
+    #region MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.gameObject.CompareTag("friend"))
+        if (col.gameObject.CompareTag("FriendRaduis"))
         {
-            col.gameObject.GetComponent<FriendController>().EnteredSpot(this);
+            col.gameObject.GetComponentInParent<FriendController>().EnteredSpot(this);
         }
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("friend"))
+        if (other.gameObject.CompareTag("FriendRaduis"))
         {
-            other.gameObject.GetComponent<FriendController>().ExitSpot(this);
+            other.gameObject.GetComponentInParent<FriendController>().ExitSpot(this);
         }
     }
+
+    #endregion MonoBehaviour
+    
+    
+    #region Methods
 
     public void HighlightSpot()
     {
@@ -27,4 +46,15 @@ public class Spot : MonoBehaviour
     {
         GetComponent<SpriteRenderer>().color = Color.white;
     }
+
+    public void InvokeEvent()
+    {
+        spotEvent.Invoke();
+    }
+    
+    #endregion
+    
+    
+
+
 }
