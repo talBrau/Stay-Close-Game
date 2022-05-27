3e1e175e921074cdb26fa082c5ec953cc8ec8e5f
talBrau <tal.brauner@mail.huji.ac.il>
2022-05-27 16:14:08 +0300

    Added text template

---
+++ Assets/Development/Script/TextReveal.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextReveal : MonoBehaviour
+{
+    private Animator _animator;
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            _animator.SetTrigger("reveal");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            _animator.SetTrigger("hide");
+        }
+    }
+}
