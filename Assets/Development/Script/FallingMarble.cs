b0cbd8f3ba74e98140090c319e3dab672cc6f98c
talBrau <tal.brauner@mail.huji.ac.il>
2022-05-27 15:50:27 +0300

    sort off finish first chapter

--- Assets/Development/Script/FallingMarble.cs
+++ Assets/Development/Script/FallingMarble.cs
@@ -5,13 +5,17 @@ public class FallingMarble : MonoBehaviour
 {
     private Vector3 initPoistion;
     private Rigidbody2D rb;
+    private SpriteRenderer _spriteRenderer;
     public bool fallen = false;
     private void Start()
     {
         initPoistion = transform.position;
         rb = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
         rb.simulated = false;
         fallen = false;
+        _spriteRenderer.color -= Color.black;
+
     }
 
     private void OnEnable()
@@ -26,6 +30,7 @@ public class FallingMarble : MonoBehaviour
 
     public void ActivateTrap()
     {
+        _spriteRenderer.color += Color.black;
         rb.simulated = true;
         // rb.velocity = Vector2.right;
     }
@@ -38,7 +43,6 @@ public class FallingMarble : MonoBehaviour
         }
         if (col.gameObject.CompareTag("Player") && !fallen)
         {
-            // Invoke("ResetPosition",0.25f);
             GameManager.CheckPointInvoke();
         }
     }
@@ -48,5 +52,7 @@ public class FallingMarble : MonoBehaviour
         rb.simulated = false;
         rb.velocity = Vector2.zero;
         transform.position = initPoistion;
+        _spriteRenderer.color -= Color.black;
+
     }
 }
