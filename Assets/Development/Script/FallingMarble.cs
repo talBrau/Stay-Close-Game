26f739cd0b4be98e1a5546d33551e3d137e25cba
talBrau <tal.brauner@mail.huji.ac.il>
2022-05-27 14:12:44 +0300

    Falling cubes stage

--- Assets/Development/Script/FallingMarble.cs
+++ Assets/Development/Script/FallingMarble.cs
@@ -5,24 +5,40 @@ public class FallingMarble : MonoBehaviour
 {
     private Vector3 initPoistion;
     private Rigidbody2D rb;
+    public bool fallen = false;
     private void Start()
     {
         initPoistion = transform.position;
         rb = GetComponent<Rigidbody2D>();
         rb.simulated = false;
+        fallen = false;
+    }
+
+    private void OnEnable()
+    {
+        GameManager.CheckPointReset += ResetPosition;
+    }
+
+    private void OnDisable()
+    {
+        GameManager.CheckPointReset -= ResetPosition;
     }
 
     public void ActivateTrap()
     {
         rb.simulated = true;
-        rb.velocity = Vector2.right;
+        // rb.velocity = Vector2.right;
     }
 
     private void OnCollisionEnter2D(Collision2D col)
     {
-        if (col.gameObject.CompareTag("Player"))
+        if (col.gameObject.CompareTag("NoJumpGround")||col.gameObject.CompareTag("Ground"))
         {
-            Invoke("ResetPosition",0.25f);
+            fallen = true;
+        }
+        if (col.gameObject.CompareTag("Player") && !fallen)
+        {
+            // Invoke("ResetPosition",0.25f);
             GameManager.CheckPointInvoke();
         }
     }
