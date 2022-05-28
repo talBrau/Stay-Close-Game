aa3f5284047f8e1a13f9769c1ccb2e34736d1a89
talBrau <tal.brauner@mail.huji.ac.il>
2022-05-28 17:15:02 +0300

    fix end game

--- Assets/ReturnFriend.cs
+++ Assets/ReturnFriend.cs
@@ -7,14 +7,22 @@ using UnityEngine;
 public class ReturnFriend : MonoBehaviour
 {
     [SerializeField] private GameObject friend;
+    private bool _done = false;
+
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.CompareTag("Player"))
         {
-            friend.GetComponent<FriendController>().friendState = FriendController.FriendState.Idle;
-            friend.transform.position = transform.position + Vector3.up;
-            // friend.GetComponent<FriendController>().IsAttracted = true;
-
+            Invoke(nameof(returnFriend), 1.5f);
         }
+    }
+
+    private void returnFriend()
+    {
+        if (_done) return;
+
+        friend.GetComponent<FriendController>().friendState = FriendController.FriendState.Idle;
+        friend.transform.position = transform.position + Vector3.up;
+        _done = true;
     }
 }
