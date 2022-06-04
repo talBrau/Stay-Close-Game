cea330def7249e0fc13584da0c46b2cabea7eef9
Arnon Guttel <arnon.guttel@mail.huji.ac.il>
2022-06-04 22:57:08 +0300

    Updated chapter 1

--- Assets/Development/Script/Camera/CinemachineChange.cs
+++ Assets/Development/Script/Camera/CinemachineChange.cs
@@ -8,9 +8,11 @@ public class CinemachineChange : MonoBehaviour
     [SerializeField] private float changeSpeed = 5;
     [SerializeField] private float orthoSize = 16;
     [SerializeField] private float followOffsetX = 7;
+    [SerializeField] private float followOffsetY = 0;
     
     private float tragetOrthoSize;
-    private float targetFollowOffsetX;
+    private float targetFollowOffsetX; 
+    private float targetFollowOffsetY;
     private CinemachineTransposer transposer;
     public bool activatedFlag;
 
@@ -19,6 +21,7 @@ public class CinemachineChange : MonoBehaviour
         transposer = CmCamera.GetCinemachineComponent<CinemachineTransposer>();
         tragetOrthoSize = orthoSize;
         targetFollowOffsetX = followOffsetX;
+        targetFollowOffsetY = followOffsetY != 0 ? followOffsetY : transposer.m_FollowOffset.y;
     }
 
     private void Update()
@@ -26,7 +29,8 @@ public class CinemachineChange : MonoBehaviour
         if (!activatedFlag) return;
         
         if (CmCamera.m_Lens.OrthographicSize != tragetOrthoSize ||
-            transposer.m_FollowOffset.x != targetFollowOffsetX)
+            transposer.m_FollowOffset.x != targetFollowOffsetX ||
+            transposer.m_FollowOffset.y != targetFollowOffsetY)
         {
             updateCameraValues();
         }
@@ -63,6 +67,19 @@ public class CinemachineChange : MonoBehaviour
             transposer.m_FollowOffset.x -= changeSpeed * Time.deltaTime;
             if (transposer.m_FollowOffset.x < targetFollowOffsetX)
                 transposer.m_FollowOffset.x = targetFollowOffsetX;
+        }
+        
+        if (transposer.m_FollowOffset.y < targetFollowOffsetY)
+        {
+            transposer.m_FollowOffset.y += changeSpeed * Time.deltaTime;
+            if (transposer.m_FollowOffset.y > targetFollowOffsetY)
+                transposer.m_FollowOffset.y = targetFollowOffsetY;
+        }
+        else
+        {
+            transposer.m_FollowOffset.y -= changeSpeed * Time.deltaTime;
+            if (transposer.m_FollowOffset.y < targetFollowOffsetY)
+                transposer.m_FollowOffset.y = targetFollowOffsetY;
         }
     }
 
